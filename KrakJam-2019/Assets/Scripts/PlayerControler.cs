15eddcd3be10d54dd97cc711e3868d1839c19001
ZKaczynski <zbigniew.kaczynski@op.pl>
2019-01-26 10:28:46 +0100

    Zbyszek 1030 Saturnday
    Working interaction between player and enemy

--- KrakJam-2019/Assets/Scripts/PlayerControler.cs
+++ KrakJam-2019/Assets/Scripts/PlayerControler.cs
@@ -8,19 +8,54 @@ public class PlayerControler : MonoBehaviour
     public float jump;
     bool onFloor;
     Rigidbody2D rgbd;
+    BoxCollider2D swing;
 
-    public GameObject stairs;
+    public float attackCooldown;
+    public float attackDuration;
+    float timer;
+    float lastHit;
+    bool canAttack;
 
-    // Start is called before the first frame update
+     enum sideOfSwing
+    {
+        Left,Right
+    }
+     sideOfSwing side;
+
+    public GameObject SwingObject;
+    Vector2 lastPosition;
+    
     void Start()
     {
+        canAttack = true;
+        timer = 0.0f;
+        side = sideOfSwing.Right;
         rgbd = GetComponent<Rigidbody2D>();
+        swing = GetComponentInChildren<BoxCollider2D>();
         onFloor = false;
+        lastPosition = transform.position;
+
+        SwingObject.GetComponent<BoxCollider2D>().enabled = false;
+
+    }
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (timer - lastHit > attackCooldown)
+        {
+            canAttack = true;
+            lastHit = 0.0f;
+        }
+        if (timer - lastHit> attackDuration){
+            SwingObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
     void Update()
     {
         getInputs();
+        chooseSideForSwing();
     }
 
     void getInputs()
@@ -33,26 +68,45 @@ public class PlayerControler : MonoBehaviour
         else if (Input.GetAxis("Horizontal") < 0)
         {
             rgbd.AddForce(new Vector2(-1f, 0f) * speed);
-
         }
 
         if ((Input.GetAxis("Vertical") > 0) && onFloor == true)
         {
             onFloor = false;
-            rgbd.velocity= new Vector2(0f, 1f) * jump;
+            rgbd.velocity = new Vector2(0f, 1f) * jump;
         }
-
 
         else if ((Input.GetAxis("Vertical") < 0))
         {
-            stairs.GetComponent<BoxCollider2D>().enabled=false;
             rgbd.AddForce(new Vector2(0f, -1f) * jump);
         }
 
+        if (Input.GetKeyDown("space")&&canAttack)
+        {
+            
+            SwingObject.GetComponent<BoxCollider2D>().enabled = true;
+            canAttack = false;
+            lastHit = timer;
+        }
     }
-
-    void canJump()
+    void chooseSideForSwing()
     {
+        Vector2 currentPosition = transform.position;
+        float deltaX = currentPosition.x - lastPosition.x;
+        lastPosition = currentPosition;
+        if (deltaX > 0 && side==sideOfSwing.Left)
+        {
+            Vector3 revert = new Vector3(6.0f, 0.0f);
+            transform.localScale += revert;
+
+            side = sideOfSwing.Right;
+        }
+        else if (deltaX < 0 && side == sideOfSwing.Right)
+        {
+            Vector3 revert = new Vector3(6.0f, 0.0f);
+            transform.localScale -=revert;
+            side = sideOfSwing.Left;
+        }
 
     }
 
